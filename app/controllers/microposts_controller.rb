6c770021400aaadae09a1a40c9f51a5614e71643
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-08-12 11:40:05 +0000

    create

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :require_user_logged_in
-
+  before_action :correct_user, only: [:destroy]
+  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -14,11 +15,21 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+  
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    unless @micropost
+      redirect_to root_url
+    end
   end
 end
