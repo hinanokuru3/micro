b6b77004ce1e400298a64e8ffc54848a1c227b9d
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-08-17 13:42:58 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
-    @microposts = @user.microposts.order(id: :desc).page(params[:page])
-    counts(@user)
   end
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
