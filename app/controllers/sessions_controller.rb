508725f30db40cc6cbffd3c0f448fa5ac66b481a
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-08-17 14:22:28 +0000

    user signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id]=nil
-    flash['sucess']='ログアウトしました。'
+    flash[:success]='ログアウトしました。'
     redirect_to root_url
   end
 
